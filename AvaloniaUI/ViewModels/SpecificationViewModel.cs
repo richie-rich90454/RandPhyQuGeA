e0be49360c8162df88c8fd5e0997c07cc89e9567
richie-rich90454 <rjiang880@gmail.com>
2026-06-09 19:41:12 +0800

    feat: create SpecificationViewModel
    Loads part_one.txt via PartOneTextLoader, exposes Units, Topics, Skills, Templates as observable collections with IsLoaded, ErrorMessage, IsLoading properties and LoadCommand.

---
+++ AvaloniaUI/ViewModels/SpecificationViewModel.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Threading.Tasks;
+using Core.Domain;
+using Core.Interfaces;
+using ReactiveUI;
+
+using ReactiveUnit = System.Reactive.Unit;
+using DomainUnit = Core.Domain.Unit;
+
+namespace AvaloniaUI.ViewModels;
+
+public class SpecificationViewModel : ViewModelBase
+{
+    private readonly ISpecificationLoader _loader;
+
+    private bool _isLoaded;
+    private string _errorMessage = string.Empty;
+    private bool _isLoading;
+    private Specification? _specification;
+
+    public SpecificationViewModel(ISpecificationLoader loader)
+    {
+        _loader = loader;
+
+        Units = new ObservableCollection<DomainUnit>();
+        Topics = new ObservableCollection<Topic>();
+        Skills = new ObservableCollection<Skill>();
+        Templates = new ObservableCollection<QuestionTemplate>();
+
+        LoadCommand = ReactiveCommand.CreateFromTask(LoadSpecification);
+    }
+
+    public bool IsLoaded
+    {
+        get => _isLoaded;
+        set => this.RaiseAndSetIfChanged(ref _isLoaded, value);
+    }
+
+    public string ErrorMessage
+    {
+        get => _errorMessage;
+        set => this.RaiseAndSetIfChanged(ref _errorMessage, value);
+    }
+
+    public bool IsLoading
+    {
+        get => _isLoading;
+        set => this.RaiseAndSetIfChanged(ref _isLoading, value);
+    }
+
+    public Specification? Specification
+    {
+        get => _specification;
+        set => this.RaiseAndSetIfChanged(ref _specification, value);
+    }
+
+    public ObservableCollection<DomainUnit> Units { get; }
+    public ObservableCollection<Topic> Topics { get; }
+    public ObservableCollection<Skill> Skills { get; }
+    public ObservableCollection<QuestionTemplate> Templates { get; }
+
+    public ReactiveCommand<ReactiveUnit, ReactiveUnit> LoadCommand { get; }
+
+    private async Task LoadSpecification()
+    {
+        IsLoading = true;
+        ErrorMessage = string.Empty;
+
+        try
+        {
+            var spec = await Task.Run(() => _loader.Load("part_one.txt"));
+
+            Specification = spec;
+
+            Units.Clear();
+            foreach (var unit in spec.Units)
+                Units.Add(unit);
+
+            Topics.Clear();
+            foreach (var topic in spec.Topics)
+                Topics.Add(topic);
+
+            Skills.Clear();
+            foreach (var skill in spec.Skills)
+                Skills.Add(skill);
+
+            Templates.Clear();
+            foreach (var template in spec.Templates)
+                Templates.Add(template);
+
+            IsLoaded = true;
+        }
+        catch (Exception ex)
+        {
+            ErrorMessage = ex.Message;
+            IsLoaded = false;
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
+}
