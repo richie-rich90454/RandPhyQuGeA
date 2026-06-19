6bd4582e7a1222daf77f44635825e6c26d3183df
richie-rich90454 <rjiang880@gmail.com>
2026-06-20 07:13:49 +0800

    Add TypeScript, Tailwind, ESLint, and HTML entry config

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: ["dist", "src-tauri", "node_modules"],
+};
