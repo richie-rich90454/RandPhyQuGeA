55d20307d648beccf081b20037d27d84c48fbd6e
richie-rich90454 <rjiang880@gmail.com>
2026-06-20 08:10:30 +0800

    Register physics_core commands in Tauri invoke handler

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,17 @@
-// Icons will be added later via `npm run tauri icon`.
+﻿// Icons will be added later via `npm run tauri icon`.
 // Bundle is currently disabled in tauri.conf.json to avoid missing-icon build errors.
+
+mod commands;
 
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![
+            commands::parse_specification,
+            commands::generate_question,
+            commands::generate_batch,
+            commands::export_questions,
+            commands::get_formula_library,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
