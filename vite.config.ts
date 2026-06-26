12e246538491aa4b9eab0b9f0bad7c320e5097db
richie-rich90454 <rjiang880@gmail.com>
2026-06-26 09:51:13 -0700

    Made build working

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   clearScreen: false,
   server: {
     port: 1420,
@@ -19,7 +20,7 @@ export default defineConfig({
   },
   build: {
     target: "es2021",
-    minify: "esbuild",
+    minify: "oxc",
     sourcemap: true,
   },
 });
